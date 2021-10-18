fc590e9bd3d0c3d17a7dfded0bacdc04f75ad6dd
unknown <mastr_sensei@protonmail.com>
2021-10-18 21:26:38 +0300

    Node Server updates

---
+++ pr0tag0nists/server.js
@@ -0,0 +1,21 @@
+// Initialize server
+var express = require('express');
+var app = express();
+const router = express.Router();
+
+// Setup routes
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+// Run Server
+app.use(express.static(__dirname));
+app.use('/', router);
+app.listen("8080", function () {
+  console.log('Example app listening on port 8080!')
+})

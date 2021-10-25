b071772c659da5b7b1ce378598a60b4957fa4d5b
unknown <mastr_sensei@protonmail.com>
2021-10-25 13:49:16 +0300

    UI Updates

--- pr0tag0nists/index.js
+++ pr0tag0nists/index.js
@@ -1,67 +1,88 @@
-// Source code to interact with smart contract
+window.onload = function () {
+	connect()
+};
 
-// web3 provider with fallback for old version
-window.addEventListener('load', async () => {
-  // New web3 provider
-  if (window.ethereum) {
-      window.web3 = new Web3(ethereum);
-      try {
-          // ask user for permission
-          await ethereum.enable();
-          // user approved permission
-      } catch (error) {
-          // user rejected permission
-          console.log('user rejected permission');
-      }
-  }
-  // Old web3 provider
-  else if (window.web3) {
-      window.web3 = new Web3(web3.currentProvider);
-      // no need to ask for permission
-  }
-  // No web3 provider
-  else {
-      console.log('No web3 provider detected');
-  }
-});
-console.log (window.web3.currentProvider)
+async function connect() {
+	// Connect to wallet
+	//Get Account info
+	const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+	const account = accounts[0];
+	console.log(accounts[0]);
+	// const showAccount = document.querySelector('.showAccount');
+	// // showAccount.innerHTML = account;
+	// document.getElementById('id_login').value = account
+	// document.getElementById('id_password').value = CryptoJS.MD5(account)
+	// //CSRF for form
+	// var inputElem = document.createElement('input');
+	// inputElem.type = 'hidden';
+	// inputElem.name = 'csrfmiddlewaretoken';
+	// inputElem.value = '{{ csrf_token }}';
+	// document.getElementById('loginForm').appendChild(inputElem);
+	// document.getElementById('loginForm').submit()
+}
+// Web3 wallet sign in end
 
-// contractAddress and abi are setted after contract deploy
-var contractAddress = '0xc864D0fef177A69aFa8E302A1b90e450910A4c3E';
-var abi = JSON.parse( '[{"constant":true,"inputs":[],"name":"getInfo","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_info","type":"string"}],"name":"setInfo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]' );
 
-//contract instance
-contract = new web3.eth.Contract(abi, contractAddress);
 
-// Accounts
-var account;
-
-web3.eth.getAccounts(function(err, accounts) {
-  if (err != null) {
-    alert("Error retrieving accounts.");
-    return;
-  }
-  if (accounts.length == 0) {
-    alert("No account found! Make sure the Ethereum client is configured properly.");
-    return;
-  }
-  account = accounts[0];
-  console.log('Account: ' + account);
-  web3.eth.defaultAccount = account;
-});
-
-//Smart contract functions
-function registerSetInfo() {
-  info = $("#newInfo").val();
-  contract.methods.setInfo (info).send( {from: account}).then( function(tx) {
-    console.log("Transaction: ", tx);
-  });
-  $("#newInfo").val('');
+// Function used to sign the raffle purchase confirmation
+async function signature() {
+	try {
+		// const msg = 'Sample message to hash for signature'
+		// const msgHash = keccak256(msg)
+		const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+		const from = accounts[0];
+		const msg = 'Purchase Raffle tickets';
+		// const msg = `0x${Buffer.from(plainMessage, 'utf8').toString('hex')}`;
+		const sign = await ethereum.request({
+			method: 'personal_sign',
+			params: [msg, from, 'Example password'],
+		});
+	} catch (err) {
+		console.error(err);
+	}
+	document.getElementById('purchase_raffle_tickets').disabled = false;			
 }
 
-function registerGetInfo() {
-  contract.methods.getInfo().call().then( function( info ) {
-    console.log("info: ", info);
-    document.getElementById('lastInfo').innerHTML = info;
-  });
+
+
+// Function used to mint protagonists
+async function minter(){
+	var amount = document.getElementById('id_contribution_amount').value;
+
+	const web3 = new Web3(window.web3.currentProvider);
+	const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+	web3.eth.defaultAccount = accounts[0];
+
+	// var Pr0tAddress = "0x83BE666f79E380070F5951b9b99efc981373B495";
+	// var Pr0tABI = [];
+
+	// var Pr0tContract = new web3.eth.Contract(Pr0tABI, Pr0tAddress);
+
+	// var today = Date.now();
+	// var slug = "{{object.slug}}";
+
+	// // Variable formating
+	// today = new Date(today).getTime() / 1000;
+	// amount = web3.utils.toWei(amount, 'ether');
+
+	// console.log("Big Number: " + amount);
+
+	// // To bytes32
+	// slug = web3.utils.toHex(slug);
+
+	// // console.log(parseFloat(requiredContribution));
+	// // console.log(deadline);
+	// // console.log(slug);
+
+
+	// var mintPr0t = Pr0tContract.methods.mintPr0t(slug, today).call().then(function (response) {
+		
+	// });
+
+	// web3.eth.sendTransaction({ to: Pr0tAddress,
+	// 			value: amount,  
+	// 			from: accounts[0], 
+	// 			gasLimit: 150000,
+	// 			data: mintPr0t });
 }
